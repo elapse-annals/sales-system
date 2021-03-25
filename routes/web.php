972b0a30b09472d70ddafe043b4e4c4194d32da5
Ben.Huang <benhuang1024@gmail.com>
2021-03-25 18:30:56 +0800

    todo

---
+++ routes/web.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Auth::routes();
+
+Route::get('/', 'ClosureController@welcome');
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+//Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
+
+Route::get('plural/{singular}', 'StringController@plural');
+
+Route::prefix('login')->group(function () {
+    Route::get('google', 'Auth\LoginController@redirectToProvider');
+    Route::get('google/callback', 'Auth\LoginController@handleProviderCallback');
+});
+
+/**
+ * Export routing registration
+ */
+Route::prefix('export')->group(function () {
+    Route::get('tmpls', 'TmplController@export');
+});
+
+/**
+ * Resource route
+ */
+//Route::resource('languages', 'LanguageController');
+
+Route::resource('tmpls', 'TmplController');
+
+Route::get('hello', 'ClosureController@hello');
+
+
+
+
